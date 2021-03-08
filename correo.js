d9be91b38c4ebae2d5510f69d659eb3cb19e1d45
bryanpumax <50306574+bryanpumax@users.noreply.github.com>
2021-03-08 17:36:34 -0500

    Update correo.js

--- correo.js
+++ correo.js
@@ -1,4 +1,4 @@
-  var email = document.getElementById("correo_docente").value;
+  var email = document.getElementById("correo").value;
     var formato = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
     var v_email = formato.test(email);
     if (v_email != true || email == "") {
